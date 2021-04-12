b3a330ac4d03ce73a96a325f31c3a79bd8d401d2
= <=>
2021-04-12 19:06:19 +0700

    update

--- addons/odootycoon/models/models.py
+++ addons/odootycoon/models/models.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError, Warning
+from random import randint 
 
 class odootycoon_gamemanager(models.Model):
-    _name = 'odootycoon.gamemanager'
-    name = fields.Char("Game Name")
-    day = fields.Integer("Current day")
-    cash = fields.Float("Cash")
+    _name = "odootycoon.gamemanager"
+    _description = "My Odoo project"
 
+    name = fields.Char("Game Name", default="New Game")
+    day = fields.Integer("Current day", default=1)
+    cash = fields.Float("Cash", default=1000)
 
+# Button: Next day
+    def nextday(self):
+        #Process Unlocked Products
+        products = self.env['product.template'].search([('unlocked','=',True)])
+        cash = 0
+        for product in products:
+            #list_price is the Sale Price of each product
+            #Must import the library's name is "random" before use syntax below
+            numsold = randint(5,35)
+            cash += product.list_price *numsold
+        self.write({'day': self.day + 1, 'cash': self.cash + cash})
+    def skip5days(self):
+        for i in range(0,5):
+            self.nextday()
+    def skip30days(self):
+        for i in range(0,30):
+            self.nextday()
+    def resetgame(self):
+        self.day = 1
+        self.cash = 1000
+        products = self.env['product.template'].search([('unlocked','=',True)]).write({'unlocked':False})
 
